899aad6a538e058b4278debff32ceaebc40dbdd6
Dylan Armstrong | Pentagon Technology <dylan.armstrong@pentagontechnology.net>
2023-05-13 03:29:42 +1000

    Started work on frontend - map and fetch api parts

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Item from "./Item";
 
 function App() {
+  const [items, setItems] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/items")
+      .then(response => response.json())
+      .then(data => {
+        setItems(data);
+      })
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="items">
+        { items.map((item, i) => <Item key={i} name={item.name} cpu={item.cpu} memory={item.memory} storage={item.storage} price={item.price}/>)}
+      </div>
     </div>
   );
 }
